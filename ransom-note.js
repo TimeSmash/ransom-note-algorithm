36c3ed89d2c29600eb7d6bf0e9b989148384bb49
TimeSmash <mcumm64@gmail.com>
2020-02-14 20:53:17 -0500

    Updated required words fxn

--- ransom-note.js
+++ ransom-note.js
@@ -1,3 +1,34 @@
+function magazineHasRequiredWords(magazine, note){
+	//takes OG arrays as args
+	//
+	let wordsBothHave = []
+	for (var i = 0; i <note.length; i++){
+		//In 1st iteration, "give" is checked against all words in magazine
+		for (var j = 0; j < magazine.length; j++){
+			if(note[i] === magazine[j] && !wordsBothHave.includes(note[i])){
+// 				console.log("push " + note[i] + "to wordsBothHave")
+				wordsBothHave.push(note[i])
+            } else {
+				null
+            }
+    	}
+    }
+// 	Confirm words in wordsBothHave match note
+	let uniqueNoteWords = Array.from(new Set(note))
+	console.log("unique", uniqueNoteWords)
+	console.log("wordsBothHave", wordsBothHave)
+	if (wordsBothHave.length === uniqueNoteWords.length){
+		return true
+    } else {
+		return false
+    }
+	
+}
+
+// At this point, have accounted for:
+// Magazine having all (WHOLE/CASE SENSITIVE) words note needs
+// BUT only for one instance of that word
+
 function getWordsAndCountsFrom(array){
     let wordsAndCounts = {}
 	for (const word of array){
@@ -11,3 +42,8 @@ function getWordsAndCountsFrom(array){
 	}
 	return wordsAndCounts
 }
+
+// Now compare the counts of the word note needs with how many present in magazine
+
+// if magazineHasRequiredWords(magazine,note) 
+
